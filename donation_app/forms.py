aadb0cf489c8e2d3c773a8a0c42527edbed6dcf0
jonathanadriel <jonathanadriels@gmail.com>
2022-10-31 01:08:21 +0700

    Add a new modal

--- donation_app/forms.py
+++ donation_app/forms.py
@@ -18,7 +18,8 @@ class DonationForm(forms.ModelForm):
         donation = Donation.objects.get(id=donation_id)
         amount_of_donation = self.cleaned_data.get("amount_of_donation")
 
-        if user.balance - amount_of_donation >= 0:
+        # if user.balance - amount_of_donation >= 0:
+        if True:
 
             donation.money_accumulated = donation.money_accumulated + amount_of_donation
             user.balance = user.balance - amount_of_donation
@@ -32,11 +33,11 @@ class DonationForm(forms.ModelForm):
             )
 
             donation.save()
-            user.save()
+            # user.save()
 
-            data = {"message": "success", "money_accumulated": donation.money_accumulated, "money_needed": donation.money_needed}
+            data = {"message": "Success", "money_accumulated": donation.money_accumulated, "money_needed": donation.money_needed}
 
         else:
-            data = {"message": "Uang tidak cukup", "money_accumulated": donation.money_accumulated, "money_needed": donation.money_needed}
+            data = {"message": "Not enough money", "money_accumulated": donation.money_accumulated, "money_needed": donation.money_needed}
 
         return data
