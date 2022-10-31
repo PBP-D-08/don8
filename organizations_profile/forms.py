0b0c7445f8fdaa44f1c02e4a7fe908488c292e55
AdyatmaNugrahaYudha <awan2403@gmail.com>
2022-10-31 14:41:20 +0700

    change minimum withdraw

--- organizations_profile/forms.py
+++ organizations_profile/forms.py
@@ -3,5 +3,5 @@ from django import forms
 class WithdrawForm(forms.Form):
     amount = forms.IntegerField(widget=forms.NumberInput(attrs={
         'class': 'bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-brokenwhite focus:border-green-medium',
-        'min' : '0',
+        'min' : '1',
     }))
