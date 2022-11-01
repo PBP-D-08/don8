2ab21c4d833a9b0dc4558ce3229deec63641ce5c
wedens.elma <wedens.elma@ui.ac.id>
2022-11-02 02:30:37 +0700

    Clean unnecessary comments and whitespace

--- profile_pengguna/forms.py
+++ profile_pengguna/forms.py
@@ -5,6 +5,4 @@ class Form(forms.Form):
     amount = forms.IntegerField(widget=forms.NumberInput(attrs={
         'class': 'bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-brokenwhite focus:border-green-medium',
         'min' : '1',}))
-    # class Meta:
-    #     model = Profile
-    #     fields = ['amount']
+        
--- profile_pengguna/models.py
+++ profile_pengguna/models.py
@@ -5,15 +5,5 @@ from donation_app.models import UserDonation
 
 # Create your models here.
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="user_profile")
-    # models.ForeignKey(User, on_delete=models.CASCADE, related_name="user_profile")
-    
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="user_profile")    
     money_donated = models.IntegerField(default=0)
-    # history = models.ForeignKey(UserDonation, on_delete=models.CASCADE, related_name="user_profile")
-
-# class Donated(models.Model):
-#     user = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name="donated")
-#     donation = models.ForeignKey(
-#         Donation, on_delete=models.CASCADE, related_name="donated"
-#     )
-
