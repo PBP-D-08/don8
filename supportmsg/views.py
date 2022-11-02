4a5980b11a33c96ecb2940a95c6a3414d13dc06b
fajarriv <rivaldiibnusina@gmail.com>
2022-11-02 08:21:59 +0700

    edit like button

--- supportmsg/views.py
+++ supportmsg/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
-from urllib import response
+from django.contrib import messages
 from django.http import HttpResponse,JsonResponse
 from django.core import serializers
 from supportmsg.models import Post
 from homepage.models import Donation
 from supportmsg.forms import PostForm
 from django.contrib.auth.decorators import login_required
+import json
+from django.core.serializers.json import DjangoJSONEncoder
 
 
 # Create your views here.
@@ -22,8 +24,32 @@ def show_support(request):
 @login_required(login_url="/auth/login/")
 def show_json(request):
     data_post = Post.objects.all()
-    # tes = Post.objects.filter(author__username = "akunbaru")
-    return HttpResponse(serializers.serialize("json", data_post), content_type="application/json")
+    res = []
+    for obj in data_post:
+        # Cek apakah user sudah like atau belum
+        if obj.likes.filter(pk=request.user.pk).exists():
+            status = True
+        else:
+            status = False
+        res.append(
+            {
+                "pk": obj.pk,
+                "fields": {
+                    # "author": obj.author,
+                    "author_name": obj.author_name,
+                    "donation_name": obj.donation_name,
+                    "message": obj.message,
+                    "date_created": obj.date_created,
+                    # "likes": obj.likes,
+                    "likes_count": obj.num_likes,
+                    "like_status": status,
+                },
+            }
+        )
+    # Sort by like count
+    res.sort(key=lambda count :count.get("fields").get("likes_count"),reverse=True)
+    return HttpResponse(json.dumps(res, indent=1, cls=DjangoJSONEncoder),content_type="application/json")
+    # return HttpResponse(serializers.serialize("json", data_post), content_type="application/json")
 
 @login_required(login_url="/auth/login/")
 def add_message(request):
@@ -37,16 +63,13 @@ def add_message(request):
         )
         post.save()
         return HttpResponse(serializers.serialize("json", [post]), content_type="application/json")
-
     return HttpResponse(content_type="application/json")
 
 @login_required(login_url="/auth/login/")
 def like_post(request):
     if request.method == "POST":
         id = request.POST.get("post_id")
-        # print(id)
         post = Post.objects.filter(pk=id)[0]
-        # print(post)
         if not post.likes.filter(pk=request.user.pk).exists():
             post.likes.add(request.user)
             post.save()
