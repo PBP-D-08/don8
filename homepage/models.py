25fd488ff193a5a55aea77f9dc00b24fe08a8994
mariana <marianasalma11@gmail.com>
2022-11-02 11:13:27 +0700

    description field to charfield w/ validator param

--- homepage/models.py
+++ homepage/models.py
@@ -1,11 +1,12 @@
 from django.conf import settings
 from django.db import models
+from django.core.validators import MinLengthValidator
 
 # Create your models here.
 class Donation(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=255)
-    description = models.TextField()
+    description = models.CharField(validators=[MinLengthValidator(38)])
     date_created = models.DateField()
     date_expired = models.DateField()
     money_accumulated = models.IntegerField(default=0)
