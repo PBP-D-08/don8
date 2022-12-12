e81deb905625ee17e8756d9fa7fa2fa8a01f3384
wedens.elma <wedens.elma@ui.ac.id>
2022-12-12 22:12:38 +0700

    Fix url

--- profile_pengguna/urls.py
+++ profile_pengguna/urls.py
@@ -10,6 +10,6 @@ urlpatterns = [
     path("<str:username>/topup/", show_json_balance, name="show_json_balance"),
     path("<str:username>/amount/", show_json_amount, name="show_json_amount"),
     path("<str:username>/flutter-history/", flutter_history, name="flutter_history"),
-    path("<str:username>/flutter_top_up/", flutter_top_up, name="flutter_top_up"),
+    path("<str:username>/flutter-top-up/", flutter_top_up, name="flutter_top_up"),
     
 ]
